e40c9d42e776be6d345910ea9bf58bcaa6dac9be
unknown <bitpit11@wp.pl>
2023-04-26 17:09:23 +0200

    control motrols speed with ps3analog

---
+++ controll_motors.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+
+class motor:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        self.in1 = 19
+        self.in2 = 13
+        self.in3 = 6
+        self.in4 = 5
+        self.en = 26
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.in3, GPIO.OUT)
+        GPIO.setup(self.in4, GPIO.OUT)
+        GPIO.setup(self.en, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.en, 1000)
+
+    def pwm_speed(self, value):
+        scaled_value = int(value / 2.55)
+        print(scaled_value)
+        self.pwm.start(scaled_value)
+        return scaled_value
+
+    def forward(self):
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
+        GPIO.output(self.in3, GPIO.HIGH)
+        GPIO.output(self.in4, GPIO.LOW)
+
+    def backward(self):
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.HIGH)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.HIGH)
+
+    def stop(self):
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+        GPIO.output(self.in3, GPIO.LOW)
+        GPIO.output(self.in4, GPIO.LOW)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+from controll_motors import motor
 
 # 15.53
 # power battery 10.95 V
@@ -46,32 +47,13 @@ axis_code = {
 # in4 blue
 # en bronze
 
-in1 = 19
-in2 = 13
-in3 = 6
-in4 = 5
-en = 26
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(in3, GPIO.OUT)
-GPIO.setup(in4, GPIO.OUT)
-GPIO.setup(en, GPIO.OUT)
 
-GPIO.output(in1, GPIO.LOW)
-GPIO.output(in2, GPIO.LOW)
-GPIO.output(in3, GPIO.LOW)
-GPIO.output(in4, GPIO.LOW)
-
-pwn_mode = GPIO.PWM(en, 1000)
-
-pwn_mode.start(25)
-
+motor = motor()
 
 for event in gamepad.read_loop():
+
     # Odczytaj dane typu "key" lub "absolute"
     if event.type == ecodes.EV_KEY or event.type == ecodes.EV_ABS:
         # Konwertuj kod klawisza na jego nazwę
@@ -82,26 +64,38 @@ for event in gamepad.read_loop():
 
             if button_name == "Cross" and event.value == 1:
                 print(" press")
-                GPIO.output(in1, GPIO.HIGH)
-                GPIO.output(in3, GPIO.HIGH)
+
 
             elif button_name == "Cross" and event.value == 0:
                 print(" no press")
-                GPIO.output(in1, GPIO.LOW)
-                GPIO.output(in3, GPIO.LOW)
 
         elif event.code in axis_code:
             axis_name = axis_code[event.code]
 
-            if axis_name == "Left stick vertical" and event.value == 0:
-                print("gora")
-                GPIO.output(in1, GPIO.HIGH)
-                GPIO.output(in3, GPIO.HIGH)
+            if axis_name == "Left stick vertical":
 
-            if axis_name == "Left stick vertical" and event.value == 255:
-                print("dol")
-                GPIO.output(in1, GPIO.LOW)
-                GPIO.output(in3, GPIO.LOW)
+                value = motor.pwm_speed(event.value)
+
+                if value < 46.0:
+                    print("forward")
+                    motor.forward()
+
+                elif value > 54:
+                    print("backward")
+                    motor.backward()
+
+                else:
+                    motor.stop()
+
+            # if axis_name == "Left stick vertical" and event.value == 0:
+            #     print("gora")
+            #     GPIO.output(in1, GPIO.HIGH)
+            #     GPIO.output(in3, GPIO.HIGH)
+            #
+            # if axis_name == "Left stick vertical" and event.value == 255:
+            #     print("dol")
+            #     GPIO.output(in1, GPIO.LOW)
+            #     GPIO.output(in3, GPIO.LOW)
 
 
 
