311be3620b393dec5f3b55010bd44f7886964304
unknown <bitpit11@wp.pl>
2023-04-28 14:27:40 +0200

    add bash script for rasbbery

---
+++ configure_rasberry.sh
@@ -0,0 +1,36 @@
+sudo apt update
+sudo apt upgrade
+
+# ps3 detect address with usb cable
+sudo apt install libusb-dev
+mkdir ~/sixpair
+cd ~/sixpair
+wget http://www.pabr.org/sixlinux/sixpair.c
+gcc -o sixpair sixpair.c -lusb
+sudo ~/sixpair/sixpair
+
+# bluetooth 
+sudo apt-get install bluetooth bluez blueman
+# sudo bluetoothctl
+#     agent on
+#     default-agent
+#     scan on
+
+#     # use your deivce address mac should display after scan on command 
+#     pair 00:1A:7D:DA:71:11
+
+#     scan off
+
+
+# list all connected device 
+ls /dev/input
+
+# # for test 
+# cat /dev/input/event1
+
+
+
+# PYTHON dependecies
+# tool for handle ps3 events
+sudo apt-get install python-evdev
+
