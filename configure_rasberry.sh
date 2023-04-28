a9ff58967777e1461233fcc450efd67b9f86ed3e
unknown <bitpit11@wp.pl>
2023-04-28 14:59:10 +0200

    fix bash

--- configure_rasberry.sh
+++ configure_rasberry.sh
@@ -11,15 +11,19 @@ sudo ~/sixpair/sixpair
 
 # bluetooth 
 sudo apt-get install bluetooth bluez blueman
-sudo bluetoothctl
-	agent on
-	default-agent
-	scan on
+sudo bluetoothctl <<EOF
+agent on
+default-agent
+scan on
+pair 00:1A:7D:DA:71:11
+scan off
+exit
+EOF
 
-# use your deivce address mac should display after scan on command 
-	pair 00:1A:7D:DA:71:11
-	scan off
-	exit
+if [ $? -eq 0 ]; then
+    echo "Find and add ps3 pad ."
+else
+    echo "Error bad ps3 mac address or "
 
 
 # list all connected device 
