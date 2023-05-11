8586dd85a07001a504f5167bc2e6406e90c33f40
unknown <bitpit11@wp.pl>
2023-05-11 23:39:54 +0200

    led test

--- test_script/led_test.py
+++ test_script/led_test.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)   # Ustawienie trybu numeracji pinów
 GPIO.setup(9, GPIO.OUT)   # Ustawienie pinu 11 jako wyjście
 
 while True:
-    GPIO.output(11, GPIO.HIGH)   # Ustawienie stanu "HIGH"
+    GPIO.output(9, GPIO.HIGH)   # Ustawienie stanu "HIGH"
     time.sleep(5)                # Czekaj 5 sekund
-    GPIO.output(11, GPIO.LOW)    # Ustawienie stanu "LOW"
+    GPIO.output(9, GPIO.LOW)    # Ustawienie stanu "LOW"
     time.sleep(5)                # Czekaj kolejne 5 sekund
